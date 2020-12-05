c1e68119647010be3a8c372aa83e3667d56021b4
Moha <mohacyrmsgr@gmail.com>
2020-12-04 22:03:09 -0600

    Multiply, add, minus and decimal  functionalities added

--- main.js
+++ main.js
@@ -1,67 +1,67 @@
-const operacion = document.getElementById("operacion");
+const operacion = document.querySelector(".operacion");
 const resultado = document.getElementById("resultado");
 const calculadora = document.querySelector(".calc-box");
 const buttons = calculadora.querySelector(".buttons-box");
-
-// buttons.addEventListener("click", (e) => {
-//   if (e.target.matches("button")) {
-//     const key = e.target;
-//     const action = key.dataset.action;
-//     if (!action) {
-//       console.log("Number key");
-//     } else if (
-//       action === "add" ||
-//       action === "minus" ||
-//       action === "multiply" ||
-//       action === "divide"
-//     ) {
-//       console.log("Operator key");
-//     } else if (action === "decimal") {
-//       console.log("Decimal Key");
-//     } else if (action === "clear") {
-//       console.log("Clear Key");
-//     } else if (action == "equal") {
-//       console.log("Equal Key");
-//     }
-//   }
-// });
+const op = ["add", "minus", "multiply"];
+let num1 = undefined;
+let num2 = undefined;
+let state = undefined;
 
 buttons.addEventListener("click", (e) => {
   if (e.target.matches("button")) {
     const key = e.target;
     const action = key.dataset.action;
+    const keyContent = Number(key.textContent);
+    const displayedNum = operacion.textContent;
     if (action) {
       switch (action) {
         case "add":
           console.log("Add key");
+          num1 = Number(displayedNum);
+          operacion.textContent = 0;
+          state = op[0];
           break;
         case "minus":
           console.log("Minus Key");
+          num1 = Number(displayedNum);
+          operacion.textContent = 0;
+          state = op[1];
           break;
         case "multiply":
           console.log("Multiply Key");
-          break;
-        case "divide":
-          console.log("Divide Key");
+          num1 = Number(displayedNum);
+          operacion.textContent = 0;
+          state = op[2];
           break;
         case "decimal":
           console.log("Decimal key");
+          operacion.textContent = displayedNum + ".";
           break;
         case "clear":
           console.log("Clear key");
+          operacion.textContent = 0;
+          resultado.textContent = 0;
+          num1 = 0;
           break;
         case "equal":
           console.log("Equal key");
+          num2 = Number(displayedNum);
+          if (state === "add") {
+            resultado.textContent = num1 + num2;
+          } else if (state === "minus") {
+            resultado.textContent = num1 - num2;
+          } else if (state === "multiply") {
+            resultado.textContent = num1 * num2;
+          }
+          operacion.textContent = 0;
           break;
       }
     } else {
       console.log("Number key");
-      const keyContent = key.textContent;
-      const displayedNum = operacion.textContent;
       if (displayedNum === "0") {
         operacion.textContent = keyContent;
       } else {
-        operacion.textContent = displayedNum + keyContent;
+        operacion.textContent = `${displayedNum}${keyContent}`;
       }
     }
   }
